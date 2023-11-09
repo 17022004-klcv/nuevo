f0b9bb66686a1a6122ae02b363dca872d4c38cc0
javier.menjivra.2023@gmail.com <javier.menjivra.2023@gmail.com>
2023-11-09 08:20:33 -0600

    Aver

--- PRUEBA/prueba/prueba/Form1.Designer.cs
+++ PRUEBA/prueba/prueba/Form1.Designer.cs
@@ -34,12 +34,13 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.BackColor = System.Drawing.Color.Tomato;
             this.button1.Location = new System.Drawing.Point(390, 459);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(256, 60);
             this.button1.TabIndex = 0;
+            this.button1.Text = "Aceptar";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
