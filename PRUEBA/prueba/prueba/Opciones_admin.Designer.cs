1ce834d3d914d1f5b27ae6e8e03ca4a818d23846
javier.menjivra.2023@gmail.com <javier.menjivra.2023@gmail.com>
2023-11-10 07:58:20 -0600

    formulario_Opciones_admin

--- PRUEBA/prueba/prueba/Opciones_admin.Designer.cs
+++ PRUEBA/prueba/prueba/Opciones_admin.Designer.cs
@@ -31,12 +31,14 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(33, 36);
@@ -48,7 +50,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(184, 37);
+            this.label1.Location = new System.Drawing.Point(184, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(382, 24);
             this.label1.TabIndex = 0;
@@ -59,12 +61,24 @@
             this.button1.BackColor = System.Drawing.Color.Aqua;
             this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(60, 118);
+            this.button1.Location = new System.Drawing.Point(45, 133);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(640, 50);
             this.button1.TabIndex = 1;
             this.button1.Text = "Registrar entidad";
             this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aqua;
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(45, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(640, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Asignar materia a docente";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Opciones_admin
             // 
@@ -86,5 +100,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
