cd277ef79d285d59d79134cea84ff587d6878c42
javier.menjivra.2023@gmail.com <javier.menjivra.2023@gmail.com>
2023-11-10 08:26:51 -0600

    Eatos son los canvios de vini

--- PRUEBA/prueba/prueba/Asignación _materia_do.Designer.cs
+++ PRUEBA/prueba/prueba/Asignación _materia_do.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Materias = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.Materias);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(32, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(740, 385);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Cyan;
+            this.textBox1.Location = new System.Drawing.Point(35, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(668, 83);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(423, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seleccione materia que impartira el docente";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aqua;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(201, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(338, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Asignar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Materias
+            // 
+            this.Materias.BackColor = System.Drawing.Color.Aqua;
+            this.Materias.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Materias.FormattingEnabled = true;
+            this.Materias.ItemHeight = 24;
+            this.Materias.Location = new System.Drawing.Point(35, 208);
+            this.Materias.Name = "Materias";
+            this.Materias.Size = new System.Drawing.Size(668, 52);
+            this.Materias.TabIndex = 3;
+            // 
+            // Asignación__materia_do
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Asignación__materia_do";
             this.Text = "Asignación__materia_do";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox Materias;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
