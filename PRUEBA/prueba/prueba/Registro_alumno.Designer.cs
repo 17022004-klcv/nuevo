21166043cfa43edd4beb367f75aa72bbf26e1d1f
17022004-klcv <150029678+17022004-klcv@users.noreply.github.com>
2023-11-16 08:13:33 -0600

    cambio color registro alumno

--- PRUEBA/prueba/prueba/Registro_alumno.Designer.cs
+++ PRUEBA/prueba/prueba/Registro_alumno.Designer.cs
@@ -30,18 +30,18 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Moccasin;
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.BackColor = System.Drawing.Color.Silver;
             this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
@@ -53,7 +53,7 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
             this.button1.Location = new System.Drawing.Point(283, 300);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 41);
@@ -61,20 +61,14 @@
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.textBox2.Location = new System.Drawing.Point(88, 228);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(522, 29);
-            this.textBox2.TabIndex = 3;
-            // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.textBox1.Location = new System.Drawing.Point(88, 87);
+            this.textBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.Location = new System.Drawing.Point(88, 80);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(522, 29);
+            this.textBox1.Size = new System.Drawing.Size(522, 41);
             this.textBox1.TabIndex = 2;
             // 
             // label2
@@ -95,11 +89,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario que desea registrar";
             // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DarkGray;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox2.Location = new System.Drawing.Point(88, 211);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(522, 41);
+            this.textBox2.TabIndex = 5;
+            // 
             // Registro_alumno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Tomato;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
             this.Name = "Registro_alumno";
@@ -114,9 +118,9 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
