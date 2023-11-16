1db7fe08733d875a8723179734219b66cf063ef6
javier.menjivra.2023@gmail.com <javier.menjivra.2023@gmail.com>
2023-11-16 08:00:59 -0600

    prueba de cambio

--- PRUEBA/prueba/prueba/ingreso_docente.Designer.cs
+++ PRUEBA/prueba/prueba/ingreso_docente.Designer.cs
@@ -90,7 +90,7 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(139, 137);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
