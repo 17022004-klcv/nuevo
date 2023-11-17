550bb49b86444bbc9ca936d255207664fca82b30
MINEDUCYT <MINEDUCYT@DV>
2023-11-17 10:48:52 -0600

    asignar materia

--- PRUEBA/prueba/prueba/Opciones_admin.Designer.cs
+++ PRUEBA/prueba/prueba/Opciones_admin.Designer.cs
@@ -66,7 +66,7 @@
             this.button1.BackColor = System.Drawing.Color.Aqua;
             this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(34, 108);
+            this.button1.Location = new System.Drawing.Point(34, 107);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(480, 41);
