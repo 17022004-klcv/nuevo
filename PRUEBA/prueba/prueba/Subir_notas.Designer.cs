fd8470204e85929a0e41c625df8eb707147653f1
javier.menjivra.2023@gmail.com <javier.menjivra.2023@gmail.com>
2023-11-18 19:58:38 -0600

    validacion de las notas y de porsentajes

--- PRUEBA/prueba/prueba/Subir_notas.Designer.cs
+++ PRUEBA/prueba/prueba/Subir_notas.Designer.cs
@@ -48,7 +48,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.butt_Ingresar = new System.Windows.Forms.Button();
-            this.raBut_periodo4 = new System.Windows.Forms.RadioButton();
+            this.raBut_periodo3 = new System.Windows.Forms.RadioButton();
             this.raBut_periodo2 = new System.Windows.Forms.RadioButton();
             this.butt_Calcular = new System.Windows.Forms.Button();
             this.raBut_periodo1 = new System.Windows.Forms.RadioButton();
@@ -77,85 +77,96 @@
             this.panel.Controls.Add(this.label2);
             this.panel.Controls.Add(this.label1);
             this.panel.Controls.Add(this.butt_Ingresar);
-            this.panel.Controls.Add(this.raBut_periodo4);
+            this.panel.Controls.Add(this.raBut_periodo3);
             this.panel.Controls.Add(this.raBut_periodo2);
             this.panel.Controls.Add(this.butt_Calcular);
             this.panel.Controls.Add(this.raBut_periodo1);
-            this.panel.Location = new System.Drawing.Point(37, 44);
+            this.panel.Location = new System.Drawing.Point(49, 54);
+            this.panel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(723, 358);
+            this.panel.Size = new System.Drawing.Size(964, 441);
             this.panel.TabIndex = 0;
             // 
             // txt_Parcial
             // 
-            this.txt_Parcial.Location = new System.Drawing.Point(357, 230);
+            this.txt_Parcial.Location = new System.Drawing.Point(476, 283);
+            this.txt_Parcial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Parcial.Name = "txt_Parcial";
-            this.txt_Parcial.Size = new System.Drawing.Size(100, 20);
+            this.txt_Parcial.Size = new System.Drawing.Size(132, 22);
             this.txt_Parcial.TabIndex = 23;
             // 
             // txt_Porse4
             // 
-            this.txt_Porse4.Location = new System.Drawing.Point(588, 170);
+            this.txt_Porse4.Location = new System.Drawing.Point(784, 209);
+            this.txt_Porse4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Porse4.Name = "txt_Porse4";
-            this.txt_Porse4.Size = new System.Drawing.Size(60, 20);
+            this.txt_Porse4.Size = new System.Drawing.Size(79, 22);
             this.txt_Porse4.TabIndex = 21;
             // 
             // txt_Porse3
             // 
-            this.txt_Porse3.Location = new System.Drawing.Point(588, 109);
+            this.txt_Porse3.Location = new System.Drawing.Point(784, 134);
+            this.txt_Porse3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Porse3.Name = "txt_Porse3";
-            this.txt_Porse3.Size = new System.Drawing.Size(60, 20);
+            this.txt_Porse3.Size = new System.Drawing.Size(79, 22);
             this.txt_Porse3.TabIndex = 20;
             // 
             // txt_Porse2
             // 
-            this.txt_Porse2.Location = new System.Drawing.Point(291, 170);
+            this.txt_Porse2.Location = new System.Drawing.Point(388, 209);
+            this.txt_Porse2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Porse2.Name = "txt_Porse2";
-            this.txt_Porse2.Size = new System.Drawing.Size(60, 20);
+            this.txt_Porse2.Size = new System.Drawing.Size(79, 22);
             this.txt_Porse2.TabIndex = 19;
             // 
             // txt_Porse1
             // 
-            this.txt_Porse1.Location = new System.Drawing.Point(291, 106);
+            this.txt_Porse1.Location = new System.Drawing.Point(388, 130);
+            this.txt_Porse1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Porse1.Name = "txt_Porse1";
-            this.txt_Porse1.Size = new System.Drawing.Size(60, 20);
+            this.txt_Porse1.Size = new System.Drawing.Size(79, 22);
             this.txt_Porse1.TabIndex = 18;
             // 
             // txt_Nota4
             // 
-            this.txt_Nota4.Location = new System.Drawing.Point(442, 168);
+            this.txt_Nota4.Location = new System.Drawing.Point(589, 207);
+            this.txt_Nota4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Nota4.Name = "txt_Nota4";
-            this.txt_Nota4.Size = new System.Drawing.Size(100, 20);
+            this.txt_Nota4.Size = new System.Drawing.Size(132, 22);
             this.txt_Nota4.TabIndex = 17;
             // 
             // txt_Nota3
             // 
-            this.txt_Nota3.Location = new System.Drawing.Point(442, 106);
+            this.txt_Nota3.Location = new System.Drawing.Point(589, 130);
+            this.txt_Nota3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Nota3.Name = "txt_Nota3";
-            this.txt_Nota3.Size = new System.Drawing.Size(100, 20);
+            this.txt_Nota3.Size = new System.Drawing.Size(132, 22);
             this.txt_Nota3.TabIndex = 16;
             // 
             // txt_Nota2
             // 
-            this.txt_Nota2.Location = new System.Drawing.Point(158, 169);
+            this.txt_Nota2.Location = new System.Drawing.Point(211, 208);
+            this.txt_Nota2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Nota2.Name = "txt_Nota2";
-            this.txt_Nota2.Size = new System.Drawing.Size(100, 20);
+            this.txt_Nota2.Size = new System.Drawing.Size(132, 22);
             this.txt_Nota2.TabIndex = 15;
             // 
             // txt_Nota1
             // 
-            this.txt_Nota1.Location = new System.Drawing.Point(157, 105);
+            this.txt_Nota1.Location = new System.Drawing.Point(209, 129);
+            this.txt_Nota1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Nota1.Name = "txt_Nota1";
-            this.txt_Nota1.Size = new System.Drawing.Size(100, 20);
+            this.txt_Nota1.Size = new System.Drawing.Size(132, 22);
             this.txt_Nota1.TabIndex = 0;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(560, 169);
+            this.label9.Location = new System.Drawing.Point(747, 208);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(22, 18);
+            this.label9.Size = new System.Drawing.Size(28, 23);
             this.label9.TabIndex = 13;
             this.label9.Text = "%";
             // 
@@ -163,9 +174,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(560, 108);
+            this.label8.Location = new System.Drawing.Point(747, 133);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(22, 18);
+            this.label8.Size = new System.Drawing.Size(28, 23);
             this.label8.TabIndex = 12;
             this.label8.Text = "%";
             // 
@@ -173,9 +185,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(263, 169);
+            this.label7.Location = new System.Drawing.Point(351, 208);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(22, 18);
+            this.label7.Size = new System.Drawing.Size(28, 23);
             this.label7.TabIndex = 11;
             this.label7.Text = "%";
             // 
@@ -183,9 +196,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(263, 108);
+            this.label6.Location = new System.Drawing.Point(351, 133);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(22, 18);
+            this.label6.Size = new System.Drawing.Size(28, 23);
             this.label6.TabIndex = 10;
             this.label6.Text = "%";
             // 
@@ -193,9 +207,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(293, 230);
+            this.label5.Location = new System.Drawing.Point(391, 283);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(61, 18);
+            this.label5.Size = new System.Drawing.Size(76, 23);
             this.label5.TabIndex = 9;
             this.label5.Text = "Parcial:";
             // 
@@ -203,9 +218,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(378, 168);
+            this.label4.Location = new System.Drawing.Point(504, 207);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 18);
+            this.label4.Size = new System.Drawing.Size(74, 23);
             this.label4.TabIndex = 8;
             this.label4.Text = "Nota 4:";
             // 
@@ -213,9 +229,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(378, 105);
+            this.label3.Location = new System.Drawing.Point(504, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 18);
+            this.label3.Size = new System.Drawing.Size(74, 23);
             this.label3.TabIndex = 7;
             this.label3.Text = "Nota 3:";
             // 
@@ -223,9 +240,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(92, 169);
+            this.label2.Location = new System.Drawing.Point(123, 208);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 18);
+            this.label2.Size = new System.Drawing.Size(74, 23);
             this.label2.TabIndex = 6;
             this.label2.Text = "Nota 2:";
             // 
@@ -233,9 +251,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(92, 108);
+            this.label1.Location = new System.Drawing.Point(123, 133);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 18);
+            this.label1.Size = new System.Drawing.Size(74, 23);
             this.label1.TabIndex = 5;
             this.label1.Text = "Nota 1:";
             // 
@@ -243,33 +262,36 @@
             // 
             this.butt_Ingresar.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.butt_Ingresar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butt_Ingresar.Location = new System.Drawing.Point(426, 288);
+            this.butt_Ingresar.Location = new System.Drawing.Point(568, 354);
+            this.butt_Ingresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.butt_Ingresar.Name = "butt_Ingresar";
-            this.butt_Ingresar.Size = new System.Drawing.Size(116, 39);
+            this.butt_Ingresar.Size = new System.Drawing.Size(155, 48);
             this.butt_Ingresar.TabIndex = 4;
             this.butt_Ingresar.Text = "Ingresar";
             this.butt_Ingresar.UseVisualStyleBackColor = false;
             this.butt_Ingresar.Click += new System.EventHandler(this.butt_Ingresar_Click);
             // 
-            // raBut_periodo4
+            // raBut_periodo3
             // 
-            this.raBut_periodo4.AutoSize = true;
-            this.raBut_periodo4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.raBut_periodo4.Location = new System.Drawing.Point(563, 51);
-            this.raBut_periodo4.Name = "raBut_periodo4";
-            this.raBut_periodo4.Size = new System.Drawing.Size(95, 22);
-            this.raBut_periodo4.TabIndex = 3;
-            this.raBut_periodo4.TabStop = true;
-            this.raBut_periodo4.Text = "Periodo 3";
-            this.raBut_periodo4.UseVisualStyleBackColor = true;
+            this.raBut_periodo3.AutoSize = true;
+            this.raBut_periodo3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raBut_periodo3.Location = new System.Drawing.Point(751, 63);
+            this.raBut_periodo3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.raBut_periodo3.Name = "raBut_periodo3";
+            this.raBut_periodo3.Size = new System.Drawing.Size(116, 27);
+            this.raBut_periodo3.TabIndex = 3;
+            this.raBut_periodo3.TabStop = true;
+            this.raBut_periodo3.Text = "Periodo 3";
+            this.raBut_periodo3.UseVisualStyleBackColor = true;
             // 
             // raBut_periodo2
             // 
             this.raBut_periodo2.AutoSize = true;
             this.raBut_periodo2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.raBut_periodo2.Location = new System.Drawing.Point(296, 51);
+            this.raBut_periodo2.Location = new System.Drawing.Point(395, 63);
+            this.raBut_periodo2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.raBut_periodo2.Name = "raBut_periodo2";
-            this.raBut_periodo2.Size = new System.Drawing.Size(95, 22);
+            this.raBut_periodo2.Size = new System.Drawing.Size(116, 27);
             this.raBut_periodo2.TabIndex = 2;
             this.raBut_periodo2.TabStop = true;
             this.raBut_periodo2.Text = "Periodo 2";
@@ -279,9 +301,10 @@
             // 
             this.butt_Calcular.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.butt_Calcular.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butt_Calcular.Location = new System.Drawing.Point(169, 288);
+            this.butt_Calcular.Location = new System.Drawing.Point(225, 354);
+            this.butt_Calcular.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.butt_Calcular.Name = "butt_Calcular";
-            this.butt_Calcular.Size = new System.Drawing.Size(116, 39);
+            this.butt_Calcular.Size = new System.Drawing.Size(155, 48);
             this.butt_Calcular.TabIndex = 1;
             this.butt_Calcular.Text = "Calcular";
             this.butt_Calcular.UseVisualStyleBackColor = false;
@@ -291,9 +314,10 @@
             // 
             this.raBut_periodo1.AutoSize = true;
             this.raBut_periodo1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.raBut_periodo1.Location = new System.Drawing.Point(55, 51);
+            this.raBut_periodo1.Location = new System.Drawing.Point(73, 63);
+            this.raBut_periodo1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.raBut_periodo1.Name = "raBut_periodo1";
-            this.raBut_periodo1.Size = new System.Drawing.Size(95, 22);
+            this.raBut_periodo1.Size = new System.Drawing.Size(116, 27);
             this.raBut_periodo1.TabIndex = 0;
             this.raBut_periodo1.TabStop = true;
             this.raBut_periodo1.Text = "Periodo 1";
@@ -301,11 +325,12 @@
             // 
             // Subir_notas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Subir_notas";
             this.Text = "Subir_notas";
             this.panel.ResumeLayout(false);
@@ -336,7 +361,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button butt_Ingresar;
-        private System.Windows.Forms.RadioButton raBut_periodo4;
+        private System.Windows.Forms.RadioButton raBut_periodo3;
         private System.Windows.Forms.RadioButton raBut_periodo2;
         private System.Windows.Forms.Button butt_Calcular;
         private System.Windows.Forms.RadioButton raBut_periodo1;
--- PRUEBA/prueba/prueba/Subir_notas.cs
+++ PRUEBA/prueba/prueba/Subir_notas.cs
@@ -25,20 +25,15 @@ namespace prueba
 
         private void butt_Ingresar_Click(object sender, EventArgs e)
         {
+      
+
             Validacion_de_campos();
-
-
-
-
-
 
 
         }
 
         public void Validacion_de_campos()
         {
-
-
             //balidacion aqui se valida que las casillas contenga datos 
             if (!double.TryParse(txt_Nota1.Text, out double nota01) ||
                 !double.TryParse(txt_Nota2.Text, out double nota02) ||
@@ -49,20 +44,76 @@ namespace prueba
                 !double.TryParse(txt_Porse2.Text, out double porse2) ||
                 !double.TryParse(txt_Porse3.Text, out double porse3) ||
                 !double.TryParse(txt_Porse4.Text, out double porse4) ||
-                !double.TryParse(txt_Parcial.Text, out double Parcial))
+                !double.TryParse(txt_Parcial.Text, out double Parcial) ||
+
+                raBut_periodo1.Checked == false &&
+                raBut_periodo2.Checked == false &&
+                raBut_periodo3.Checked == false)
             {
                 MessageBox.Show("Llene los campos correspondientes o\nAsegurese que no vaya un dato de topo caracter");
             }
             else
             {
-                MessageBox.Show("Los cambios se an enviado correcgta mente");
+                Valiar_nota_porse();
+
+
             }
-          
+
         }
 
         private void butt_Calcular_Click(object sender, EventArgs e)
         {
             Validacion_de_campos();
+            Valiar_nota_porse();
         }
+
+        private void Valiar_nota_porse()
+        {
+            //declaracion de variables de las notas
+            double nota1 = double.Parse(txt_Nota1.Text);
+            double nota2 = double.Parse(txt_Nota2.Text);
+            double nota3 = double.Parse(txt_Nota3.Text);
+            double nota4 = double.Parse(txt_Nota4.Text);
+            double parcial = double.Parse(txt_Parcial.Text);
+            //declaracion de varibles para los porsentajes
+
+            double porsentaje1 = double.Parse(txt_Porse1.Text);
+            double porsentaje2 = double.Parse(txt_Porse2.Text);
+            double porsentaje3 = double.Parse(txt_Porse3.Text); 
+            double porsentaje4 = double.Parse(txt_Porse4.Text); 
+
+            //sesuma los porsentajes para ver el valor que suman 
+            double suma_porsentaje = 0;
+            suma_porsentaje = (porsentaje1 + porsentaje2 + porsentaje3 + porsentaje4);
+
+            //validacion del posentaje 
+            if (suma_porsentaje == 50)
+            {
+
+                //validacion de notas 
+                if (nota1 >= 0 && nota1 <= 10 &&
+                    nota2 >= 0 && nota2 <= 10 &&
+                    nota3 >= 0 && nota3 <= 10 &&
+                    nota4 >= 0 && nota4 <= 10 &&
+                    parcial >=0 && parcial <=10)
+                {
+
+                    MessageBox.Show("Los cambios se an enviado correcgta mente");
+                }
+                else
+                {
+                    MessageBox.Show("La nota esta fuera de ragango");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("El posentaje tiene que ser igual a 50% ");
+            }
+        }
+
+
+
+
     }
 }
