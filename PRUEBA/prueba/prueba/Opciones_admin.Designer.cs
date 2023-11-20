1da45ec10bbf6dab51ad2016ac72716a7bdbdb7b
MINEDUCYT <MINEDUCYT@DV>
2023-11-20 06:43:20 -0600

    Hoy si final

--- PRUEBA/prueba/prueba/Opciones_admin.Designer.cs
+++ PRUEBA/prueba/prueba/Opciones_admin.Designer.cs
@@ -91,9 +91,9 @@
             // 
             this.button2.BackColor = System.Drawing.Color.DarkGray;
             this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 20);
+            this.button2.Location = new System.Drawing.Point(12, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(44, 33);
+            this.button2.Size = new System.Drawing.Size(46, 36);
             this.button2.TabIndex = 1;
             this.button2.Text = "<";
             this.button2.UseVisualStyleBackColor = false;
