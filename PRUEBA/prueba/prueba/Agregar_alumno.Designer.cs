eedd8c94634cf8d2e7fcf13293eacb15504227c0
javier.menjivra.2023@gmail.com <javier.menjivra.2023@gmail.com>
2023-11-21 13:54:31 -0600

    prueva error

--- PRUEBA/prueba/prueba/Agregar_alumno.Designer.cs
+++ PRUEBA/prueba/prueba/Agregar_alumno.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
             this.comboBoxM = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.txt_name_Estudiante = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,10 +47,24 @@
             this.panel1.Controls.Add(this.txt_name_Estudiante);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(43, 40);
+            this.panel1.Location = new System.Drawing.Point(57, 49);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(716, 369);
+            this.panel1.Size = new System.Drawing.Size(955, 454);
             this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGray;
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(20, 18);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 47);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBoxM
             // 
@@ -63,19 +77,21 @@
             "Logica de programación",
             "Matematica I",
             "Sistemas informaticos"});
-            this.comboBoxM.Location = new System.Drawing.Point(130, 233);
+            this.comboBoxM.Location = new System.Drawing.Point(173, 287);
+            this.comboBoxM.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxM.Name = "comboBoxM";
-            this.comboBoxM.Size = new System.Drawing.Size(530, 26);
+            this.comboBoxM.Size = new System.Drawing.Size(705, 31);
             this.comboBoxM.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkGray;
             this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(301, 302);
+            this.button1.Location = new System.Drawing.Point(399, 373);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
             this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.button1.Size = new System.Drawing.Size(107, 36);
+            this.button1.Size = new System.Drawing.Size(143, 44);
             this.button1.TabIndex = 3;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = false;
@@ -86,10 +102,11 @@
             this.txt_name_Estudiante.BackColor = System.Drawing.Color.DarkGray;
             this.txt_name_Estudiante.Cursor = System.Windows.Forms.Cursors.Hand;
             this.txt_name_Estudiante.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_name_Estudiante.Location = new System.Drawing.Point(130, 106);
+            this.txt_name_Estudiante.Location = new System.Drawing.Point(173, 130);
+            this.txt_name_Estudiante.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_name_Estudiante.Multiline = true;
             this.txt_name_Estudiante.Name = "txt_name_Estudiante";
-            this.txt_name_Estudiante.Size = new System.Drawing.Size(535, 38);
+            this.txt_name_Estudiante.Size = new System.Drawing.Size(712, 46);
             this.txt_name_Estudiante.TabIndex = 2;
             this.txt_name_Estudiante.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -97,9 +114,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(47, 193);
+            this.label2.Location = new System.Drawing.Point(63, 238);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(172, 22);
+            this.label2.Size = new System.Drawing.Size(217, 27);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre de materia";
             // 
@@ -107,31 +125,21 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(47, 70);
+            this.label1.Location = new System.Drawing.Point(63, 86);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 22);
+            this.label1.Size = new System.Drawing.Size(256, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre del estudiante";
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.DarkGray;
-            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(15, 15);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(36, 38);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "<";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Agregar_alumno
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Agregar_alumno";
             this.Text = "Agregar_alumno";
             this.panel1.ResumeLayout(false);
